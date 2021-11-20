abd668876d87921e46195c2fffb94d9ddb1d1d16
Mouadzz <mlasrite@student.1337.ma>
2021-11-20 16:22:15 +0100

    .

--- server/server.cpp
+++ server/server.cpp
@@ -6,99 +6,27 @@
 /*   By: mlasrite <mlasrite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 18:51:55 by mlasrite          #+#    #+#             */
-/*   Updated: 2021/11/19 19:27:15 by mlasrite         ###   ########.fr       */
+/*   Updated: 2021/11/20 14:57:17 by mlasrite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.hpp"
 
-int ports[] = {8000,
-               8001,
-               8002,
-               8003};
-
-void launchServSocks(std::vector<Socket> &servSocks)
+int entry()
 {
-    for (size_t i = 0; ports[i]; i++)
-        servSocks.push_back(Socket(ports[i]));
+    Server server;
 
-    for (size_t i = 0; i < servSocks.size(); i++)
-        servSocks[i].launchSock();
-}
+    std::vector<int> ports;
+    ports.push_back(8000);
+    ports.push_back(8001);
 
-void clean(std::vector<Socket> &servSocks)
-{
-    for (size_t i = 0; i < servSocks.size(); i++)
-        servSocks[i].m_close();
-    servSocks.clear();
-}
-
-void fillSockSet(fd_set &sockSet, std::vector<Socket> &servSocks, std::vector<int> &clientSocks)
-{
-    // reset our set
-    FD_ZERO(&sockSet);
-
-    // add serv sockets
-    for (size_t i = 0; i < servSocks.size(); i++)
-        FD_SET(servSocks[i].getSockFd(), &sockSet);
-
-    // add client sockets
-    for (size_t i = 0; i < clientSocks.size(); i++)
-        FD_SET(clientSocks[i], &sockSet);
-}
-
-void acceptNewClient(Socket servSock, std::vector<int> &clientSocks)
-{
-    int newClient;
-
-    std::cout << "New Client appeared on port: " << servSock.getPort() << "\n";
-    newClient = accept(servSock.getSockFd(), 0, 0);
-    clientSocks.push_back(newClient);
-}
-
-void handleClient(int clientSock)
-{
-    std::cout << "Handle Client: " << clientSock << " !\n";
-    time_t t;
-    time(&t);
-    std::string body = ctime(&t);
-    std::string headers = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
-    headers.append(std::to_string(body.size()));
-    headers.append("\n\n");
-    char *response = strdup((headers + body).c_str());
-    send(clientSock, response, strlen(response), 0);
-}
-
-void entry()
-{
-    std::vector<Socket> servSocks;
-    std::vector<int> clientSocks;
-    fd_set sockSet;
-    int result;
-
-    launchServSocks(servSocks);
+    server.setPorts(ports);
+    server.startServSockets();
+    server.fillSockSet();
     while (1)
     {
-        fillSockSet(sockSet, servSocks, clientSocks);
-        result = select(FD_SETSIZE, &sockSet, NULL, NULL, NULL);
-        if (result == -1)
-            std::cout << "Error\n";
-        else if (result > 0)
-        {
-            // test serv sockets
-            for (size_t i = 0; i < servSocks.size(); i++)
-            {
-                if (FD_ISSET(servSocks[i].getSockFd(), &sockSet))
-                    acceptNewClient(servSocks[i], clientSocks);
-            }
-
-            // Now test the client sockets
-            for (size_t i = 0; i < clientSocks.size(); i++)
-            {
-                if (FD_ISSET(clientSocks[i], &sockSet))
-                    handleClient(clientSocks[i]);
-            }
-        }
+        server.performSelect();
     }
-    clean(servSocks);
+    server.clean();
+    return 0;
 }
--- server/server.hpp
+++ server/server.hpp
@@ -6,54 +6,125 @@
 /*   By: mlasrite <mlasrite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 18:28:22 by mlasrite          #+#    #+#             */
-/*   Updated: 2021/11/19 19:19:55 by mlasrite         ###   ########.fr       */
+/*   Updated: 2021/11/20 16:21:35 by mlasrite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include "socket.hpp"
 #include <vector>
-#define QUEUE_SIZE 10
 #define MAX_BUFFER_SIZE 1024 * 20
 
-class Socket
+class Server
 {
 private:
-    int _sockfd;
-    int _port;
-    struct sockaddr_in _serv_addr;
+    std::vector<Socket> _sockets;
+    std::vector<int> _ports;
+    fd_set _sockSet;
+    int _maxFd;
 
 public:
-    Socket(int port) : _port(port) {}
-    ~Socket() {}
+    Server() : _maxFd(0) {}
+    ~Server() { this->clean(); }
 
-    void launchSock()
+    void setPorts(std::vector<int> &ports) { this->_ports = ports; }
+
+    void startServSockets()
     {
-        // create socket
-        this->_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        for (size_t i = 0; i < this->_ports.size(); i++)
+        {
+            Socket sock(true);
+            sock.setPort(this->_ports[i]);
+            this->_sockets.push_back(sock);
+        }
 
-        // assign  Ip, convert port to network byte order and assign local address
-        this->_serv_addr.sin_family = AF_INET;
-        this->_serv_addr.sin_port = htons(this->_port);
-        this->_serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-        // asign address to socket
-        bind(this->_sockfd, (struct sockaddr *)&this->_serv_addr, sizeof(this->_serv_addr));
-
-        // prepare the server for incoming clients requests
-        listen(this->_sockfd, QUEUE_SIZE);
-        std::cout << "Server listening on port: " << this->_port << std::endl;
+        for (size_t i = 0; i < this->_sockets.size(); i++)
+        {
+            if (this->_sockets[i].isServSock())
+                this->_sockets[i].launchSock();
+        }
     }
 
-    void m_close()
+    void fillSockSet()
     {
-        close(this->_sockfd);
+        // reset our set
+        FD_ZERO(&this->_sockSet);
+
+        // add sockets to the set
+        for (size_t i = 0; i < this->_sockets.size(); i++)
+        {
+            FD_SET(this->_sockets[i].getSockFd(), &this->_sockSet);
+            if (this->_sockets[i].getSockFd() > this->_maxFd)
+                this->_maxFd = this->_sockets[i].getSockFd();
+        }
     }
 
-    int getSockFd() { return this->_sockfd; }
-    int getPort() { return this->_port; }
+    void performSelect()
+    {
+        int result = select(this->_maxFd + 1, &this->_sockSet, NULL, NULL, NULL);
+        if (result == -1)
+            std::cout << "Error\n";
+        else if (result > 0)
+        {
+            for (size_t i = 0; i < this->_sockets.size(); i++)
+            {
+                if (FD_ISSET(this->_sockets[i].getSockFd(), &this->_sockSet))
+                {
+                    if (this->_sockets[i].isServSock())
+                        acceptNewClient(this->_sockets[i]);
+                    else
+                        handleClient(this->_sockets[i]);
+                }
+            }
+        }
+    }
+
+    void acceptNewClient(Socket &sock)
+    {
+        std::cout << "New Client appeared on port: " << sock.getPort() << "\n";
+
+        int newClient = accept(sock.getSockFd(), 0, 0);
+
+        Socket client(false);
+        client.setSockFd(newClient);
+        this->_sockets.push_back(client);
+
+        FD_SET(client.getSockFd(), &this->_sockSet);
+        if (client.getSockFd() > this->_maxFd)
+            this->_maxFd = client.getSockFd();
+    }
+
+    void handleClient(Socket &client)
+    {
+        std::cout << "Handle Client: " << client.getSockFd() << " !\n";
+        char buff[MAX_BUFFER_SIZE];
+
+        int size = recv(client.getSockFd(), buff, MAX_BUFFER_SIZE, 0);
+        if (size <= 0)
+            return;
+        std::cout << "size -> " << size << std::endl;
+        sendRequest(client);
+        // FD_CLR(client.getSockFd(), &this->_sockSet);
+        // client.m_close();
+    }
+
+    void sendRequest(Socket &client)
+    {
+        time_t t;
+        time(&t);
+        std::string body = ctime(&t);
+        std::string headers = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
+        headers.append(std::to_string(body.size()));
+        headers.append("\n\n");
+        char *response = strdup((headers + body).c_str());
+        send(client.getSockFd(), response, strlen(response), 0);
+    }
+
+    void clean()
+    {
+        for (size_t i = 0; i < this->_sockets.size(); i++)
+            this->_sockets[i].m_close();
+        this->_sockets.clear();
+    }
 };
 
-void entry();
+int entry();
